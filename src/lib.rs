34031ef7e0008b0c348402f52897a2281d2b73ae
bronekot <13498924+bronekot@users.noreply.github.com>
2026-04-24 02:27:51 +0300

    library: expose conversion API

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,15 @@ mod sharpyuv;
 #[cfg(test)]
 mod integration_tests;
 
+pub use cli::{Job, Mode, ResizeFilter, ResizeOptions};
 pub use error::{Error, Result};
+pub use metadata::MetadataPolicy;
 
 pub fn run_from_env() -> Result<()> {
     let job = cli::parse_from_env()?;
+    pipeline::run(job)
+}
+
+pub fn run(job: Job) -> Result<()> {
     pipeline::run(job)
 }
